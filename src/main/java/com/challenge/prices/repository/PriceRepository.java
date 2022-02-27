70f2d16092b60c5b7dfb7f31849f4f125c5f9e7f
DESKTOP-4HC5T4Q\Na Accel One <nadia.ujovich@accelone.com>
2022-02-27 00:18:15 -0300

    Delete unused package

--- src/main/java/com/challenge/prices/repository/PriceRepository.java
+++ src/main/java/com/challenge/prices/repository/PriceRepository.java
@@ -3,7 +3,6 @@ package com.challenge.prices.repository;
 import com.challenge.prices.entity.Price;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
--- src/main/java/com/challenge/prices/service/impl/PriceServiceImpl.java
+++ src/main/java/com/challenge/prices/service/impl/PriceServiceImpl.java
@@ -7,7 +7,6 @@ import com.challenge.prices.service.PriceService;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
