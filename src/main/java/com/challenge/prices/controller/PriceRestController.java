33bd8d56bef2971b6422d3488c6c27df43440608
DESKTOP-4HC5T4Q\Na Accel One <nadia.ujovich@accelone.com>
2022-02-27 00:17:19 -0300

    Delete unused package

--- src/main/java/com/challenge/prices/controller/PriceRestController.java
+++ src/main/java/com/challenge/prices/controller/PriceRestController.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.ResponseEntity;
